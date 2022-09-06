1ce94d62f1d097afad87dd32b3a3a62b7c71591d
Bao <bao@flodesk.com>
2022-09-06 16:12:03 +0700

    projects route

--- app/root.tsx
+++ app/root.tsx
@@ -10,13 +10,13 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "bao - a guy who likes to craft ui/ux stuffs",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
