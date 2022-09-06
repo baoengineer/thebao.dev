b36bdfb6e2281482ee8419a1a6c98c60b95087f8
Bao <bao@flodesk.com>
2022-09-06 18:00:55 +0700

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,25 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = ({ env }) => {
+	const PROD = env === 'production';
+	return {
+		plugins: [
+			require('tailwindcss'),
+			require('postcss-flexbugs-fixes'),
+			require('autoprefixer'),
+			...(PROD
+				? [
+						require('cssnano')({
+							preset: [
+								'default',
+								{
+									cssDeclarationSorter: false,
+									discardComments: {
+										removeAll: true,
+									},
+								},
+							],
+						}),
+				  ]
+				: []),
+		],
+	};
 };
