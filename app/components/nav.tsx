4a2f9277d06ec011248200484eef4430113218d0
bao <thebao.developer@gmail.com>
2022-10-14 10:23:56 +0700

    update

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,29 +2,29 @@ import clsx from 'clsx';
 import { Link } from '@remix-run/react';
 
 type NavProps = {
-    className?: string;
+  className?: string;
 };
 
 export default function Nav({ className }: NavProps) {
-    return (
-        <nav
-            className={clsx(
-                'flex flex-row items-center font-primary text-primary hidden sm:flex md:text-md lg:text-lg',
-                className,
-            )}
-        >
-            <ul className="flex">
-                <Link to="/blog" className="mr-5">
-                    blog
-                </Link>
-                <a href="#about" className="mr-5">
-                    about
-                </a>
-                <Link to="/works" className="mr-5">
-                    works
-                </Link>
-                <Link to="/contact">contact</Link>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav
+      className={clsx(
+        'flex flex-row items-center font-primary text-primary hidden sm:flex md:text-md lg:text-lg',
+        className,
+      )}
+    >
+      <ul className="flex">
+        <Link to="/blog" className="mr-5">
+          blog
+        </Link>
+        <a href="#about" className="mr-5">
+          about
+        </a>
+        <Link to="/works" className="mr-5">
+          works
+        </Link>
+        <Link to="/contact">contact</Link>
+      </ul>
+    </nav>
+  );
 }
--- app/routes/[robots.txt].tsx
+++ app/routes/[robots.txt].tsx
@@ -1,7 +1,7 @@
 export const loader = () => {
-    // handle "GET" request
-    // set up our text content that will be returned in the response
-    const robotText = `
+  // handle "GET" request
+  // set up our text content that will be returned in the response
+  const robotText = `
     User-agent: Googlebot
     Disallow: /nogooglebot/
 
@@ -10,11 +10,11 @@ export const loader = () => {
 
     Sitemap: http://www.thebao.dev/sitemap.xml
     `;
-    // return the text content, a status 200 success response, and set the content type to text/plain
-    return new Response(robotText, {
-        status: 200,
-        headers: {
-            'Content-Type': 'text/plain',
-        },
-    });
+  // return the text content, a status 200 success response, and set the content type to text/plain
+  return new Response(robotText, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
 };
