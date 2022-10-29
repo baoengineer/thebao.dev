7b4ba6826508f93a1c83e6bafe79f8ecc131fbcb
bao <thebao.developer@gmail.com>
2022-10-29 09:41:09 +0700

    update

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 });
 
-const MAINTENANCE_MODE = true;
+const MAINTENANCE_MODE = false;
 export default function App() {
   const [theme, setTheme] = useState('light');
 
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,5 @@
 import anime from 'animejs';
 import { useEffect, useRef, useState } from 'react';
-import { Link } from '@remix-run/react';
 import avatar from 'public/images/avatar.png';
 import Lottie from 'lottie-react';
 import meAnimation from '../animations/me.json';
@@ -180,7 +179,7 @@ export default function Main() {
                         href="https://www.upwork.com/workwith/thebao"
                         target="_blank"
                         className="w-6 h-6 flex items-center justify-center text-gray-100 hover:text-black dark:hover:text-white transition"
-                        title="Hire me at upwork"
+                        title="Hire me at upwork" rel="noreferrer"
                       >
                         <svg
                           id="Layer_1"
@@ -203,7 +202,7 @@ export default function Main() {
                         href="https://twitter.com/thebao_dev"
                         target="_blank"
                         className="w-6 h-6 text-gray-100 hover:text-black dark:hover:text-white transition"
-                        title="Twitter"
+                        title="Twitter" rel="noreferrer"
                       >
                         <svg viewBox="0 0 512 512" className="w-6 h-6" fill="currentColor">
                           <path d="M419.6 168.6c-11.7 5.2-24.2 8.7-37.4 10.2 13.4-8.1 23.8-20.8 28.6-36 -12.6 7.5-26.5 12.9-41.3 15.8 -11.9-12.6-28.8-20.6-47.5-20.6 -42 0-72.9 39.2-63.4 79.9 -54.1-2.7-102.1-28.6-134.2-68 -17 29.2-8.8 67.5 20.1 86.9 -10.7-0.3-20.7-3.3-29.5-8.1 -0.7 30.2 20.9 58.4 52.2 64.6 -9.2 2.5-19.2 3.1-29.4 1.1 8.3 25.9 32.3 44.7 60.8 45.2 -27.4 21.4-61.8 31-96.4 27 28.8 18.5 63 29.2 99.8 29.2 120.8 0 189.1-102.1 185-193.6C399.9 193.1 410.9 181.7 419.6 168.6z" />
@@ -216,7 +215,7 @@ export default function Main() {
                         href="https://www.linkedin.com/in/thebao/"
                         target="_blank"
                         className="w-6 h-6 text-gray-100 hover:text-black dark:hover:text-white transition"
-                        title="LinkedIn"
+                        title="LinkedIn" rel="noreferrer"
                       >
                         <svg viewBox="0 0 512 512" className="w-6 h-6" fill="currentColor">
                           <path d="M186.4 142.4c0 19-15.3 34.5-34.2 34.5 -18.9 0-34.2-15.4-34.2-34.5 0-19 15.3-34.5 34.2-34.5C171.1 107.9 186.4 123.4 186.4 142.4zM181.4 201.3h-57.8V388.1h57.8V201.3zM273.8 201.3h-55.4V388.1h55.4c0 0 0-69.3 0-98 0-26.3 12.1-41.9 35.2-41.9 21.3 0 31.5 15 31.5 41.9 0 26.9 0 98 0 98h57.5c0 0 0-68.2 0-118.3 0-50-28.3-74.2-68-74.2 -39.6 0-56.3 30.9-56.3 30.9v-25.2H273.8z" />
@@ -229,7 +228,7 @@ export default function Main() {
                         href="https://www.instagram.com/the.baooo/"
                         target="_blank"
                         className="w-6 h-6 text-gray-100 hover:text-black dark:hover:text-white transition"
-                        title="Instagram"
+                        title="Instagram" rel="noreferrer"
                       >
                         <svg viewBox="0 0 512 512" className="w-6 h-6" fill="currentColor">
                           <g>
@@ -304,7 +303,7 @@ export default function Main() {
                   <a
                     className="relative hover:underline underline-offset-4 before:content-[' '] before:block before:opacity-50 before:text-gray-200 before:bg-gray-200 before:absolute before:-top-4 before:-left-4 before:w-12 before:h-12 before:rounded-full before-p-4 before:hover:w-16 before:hover:h-16 before:transition-all"
                     href="https://flodesk.com"
-                    target="_blank"
+                    target="_blank" rel="noreferrer"
                   >
                     <span className="relative z-10 font-primary">view project →</span>
                   </a>
@@ -402,7 +401,10 @@ export default function Main() {
           </li>
         </ul>
         <div className="mt-4 py-8 lg:py-12 text-lg md:text-xl lg:text-2xl">
-          <div className="container flex items-center md:items-end text-gray-200" data-aos="fade-up">
+          <div
+            className="container flex items-center md:items-end text-gray-200"
+            data-aos="fade-up"
+          >
             <a
               className="flex items-end hover:opacity-70 hover:underline transition-all"
               href="https://github.com/thebaodev"
